530f7c5e4bd96a2eb971d3dd05f7bc2b5619444d
L0ckhEEd_M4rt1N <89923719+FullMetal128@users.noreply.github.com>
2025-02-10 12:22:04 +0300

    Update block.py
    5 min block

--- block.py
+++ block.py
@@ -1,4 +1,3 @@
-import json
 import sys
 import paramiko
 from datetime import datetime, timedelta
@@ -6,25 +5,35 @@ import time
 import requests
 import urllib3
 
-host_asa = sys.argv[1] 
-user_asa = sys.argv[2] 
-password_asa = sys.argv[3] 
-port_asa = sys.argv[4] 
+host_asa = sys.argv[1] # '10.41.53.253'  заменен на переменные sys.argv[1]
+user_asa = sys.argv[2] #'admin'  заменен на переменные sys.argv[2]
+password_asa = sys.argv[3] #'1234567890' в коде заменены на переменные sys.argv[3]
+port_asa = sys.argv[4] #22 заменен на переменные sys.argv[4]
 
-incident = sys.argv[5] #'{{tag.IDENTIFIER}}' 
-XTOKEN = sys.argv[6] 
-PROTOCOL = sys.argv[7] # 'http://' 
-RVISION = sys.argv[8] #  ip r-vision
+incident = sys.argv[5] #'{{tag.IDENTIFIER}}'  sys.argv[5] 
+XTOKEN = sys.argv[6] #'78479506a4173b34305b9168ea15b71a839cadca3698dc70185f3c742f58032d' заменен на переменные sys.argv[6]
+PROTOCOL = sys.argv[7] # 'http://' заменен на переменные sys.argv[7]
+RVISION = sys.argv[8] # '10.22.20.140' заменен везде на переменные sys.argv[8]
 
 
 def get_cisco_info(protocol: str, rvision: str, XToken: str, incident:str) -> list:
     requests.packages.urllib3.disable_warnings()
     s = requests.Session()
+    #фильтр в инцидентах заменить
     incidents = s.get(protocol + rvision + '/api/v2/incidents' + '?filter=[{\"property\":\"identifier\",\"operator\":\"=\",\"value\":\"' + incident + '\"}]',
                       headers={'X-Token': XToken},
                       verify=False)
     incidentsResult = incidents.json()
     return incidentsResult['data']['result'][0]['cisco_integration']
+
+def extract_ip_addresses(text):
+    # Регулярное выражение для поиска IP-адресов
+    ip_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
+    # Поиск всех IP-адресов в строке
+    ip_addresses = re.findall(ip_pattern, text)
+    # Фильтруем только валидные IP-адреса (0-255 для каждого октета)
+    valid_ips = [ip for ip in ip_addresses if all(0 <= int(octet) <= 255 for octet in ip.split('.'))]
+    return valid_ips
 
 def send_command(cmd):
     client.send(cmd + '\n')
@@ -68,11 +77,16 @@ def add_ip_to_ACL(passw, incident):
             update_bad()
         else:
             for i in get_list_of_IP(incident):
-                send_command(f'access-list TEST2 line 1 extended deny ip host {i} any time-range {incident}')
+                send_command(f'access-list TEST2 line 1 extended deny ip host {i} any time-range {incident}') #подумать
                 send_command(f'access-list PC2 line 1 extended deny ip any host {i} time-range {incident}')
     except:
         None
 
+
+def show_time_range(passw) -> list:
+    send_command('en')
+    send_command(passw)
+    return send_command('sh time-range')
 
 def show_ACL(passw) -> list:
     send_command('en')
@@ -85,29 +99,54 @@ def delete_time_range(passw):
     send_command('en')
     send_command(passw)
     send_command('conf t')
-    send_command('clear configur time-range')
+    time_range = send_command('sh time-range')
+    if any('(active)' in i.lower().split() for i in time_range):
+        return None
+    else:
+        send_command('clear configur time-range')
+
+
+def delete_from_ACL_first(passw):
+
+    data_time_range = show_time_range(passw)
+    counter = 0
+    for i in data_time_range:
+        if '(active)' in i:
+            counter += 1
+
+    if counter == 0:
+        for i in show_ACL(passw):
+            if '224' in i and '(inactive)' in i: # 224 название ACL поменять на нужный
+                send_command('en')
+                send_command(passw)
+                send_command('conf t')
+                rez = extract_ip_addresses(i) #no access-list 224 line 1 extended deny ip host rez[0] any time-range 225
+                send_command(f'no access-list 224 extended deny ip host {rez[0]} any')
+        delete_time_range(passw)
+    else:
+        return None
 
 
 
 def update_good(incident: str):
     DATA_EXPORT = get_cisco_info(protocol=PROTOCOL, rvision=RVISION, XToken=XTOKEN, incident= incident)
     DATA_IMPORT = DATA_EXPORT
-
+    
     for i in DATA_IMPORT:
         if i['block_ip'] == True:
             i['status_kill_session'] = 'IP адрес заблокирован'
-
+    
     data = {'identifier': incident, 'cisco_integration': DATA_IMPORT}
     requests.post(PROTOCOL + RVISION + '/api/v2/incidents', headers={'X-Token': XTOKEN}, data=json.dumps(data), verify=False)
 
-def update_bad(incident: str):
+def update_bad():
     DATA_EXPORT = get_cisco_info(protocol=PROTOCOL, rvision=RVISION, XToken=XTOKEN, incident=incident)
     DATA_IMPORT = DATA_EXPORT
-
+    
     for i in DATA_IMPORT:
         if i['block_ip'] == True:
             i['status_kill_session'] = 'Ошибка при блокировке'
-
+    
     data = {'identifier': incident, 'cisco_integration': DATA_IMPORT}
     requests.post(PROTOCOL + RVISION + '/api/v2/incidents', headers={'X-Token': XTOKEN}, data=json.dumps(data), verify=False)
 
@@ -120,15 +159,15 @@ try:
     client_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client_pre.connect(hostname=host_asa, username=user_asa, password=password_asa, port=port_asa)
     client = client_pre.invoke_shell()
-    #functions
-
-
+                #functions
+    delete_from_ACL_first(password_asa) #надо проверить
+    
     add_time_range(a, password_asa, incident)
     add_ip_to_ACL(password_asa, incident)
     update_good(incident)
-
+    
 
     client.close()
     client_pre.close()
 except:
-    update_bad(incident)
+    update_bad()
